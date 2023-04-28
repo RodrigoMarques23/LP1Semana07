093c2ae51bef8482d67549565802a22734e2a2d3
Butchsss <rodrigomarques23work@gmail.com>
2023-04-28 13:48:43 +0100

    vars add

---
+++ JustScored/PlayerInfo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace JustScored
+{
+    public class PlayerInfo
+    {
+
+        private float maxScore;
+        private int gamesPlayed;
+        private int gamesWon;
+    }
+
+}
