3bd247eebccb7650febf25bc57334b909814ad8a
Butchsss <rodrigomarques23work@gmail.com>
2023-04-28 14:05:35 +0100

    methods add

--- JustScored/PlayerInfo.cs
+++ JustScored/PlayerInfo.cs
@@ -6,8 +6,42 @@ namespace JustScored
     {
 
         private float maxScore;
+        private string name;
         private int gamesPlayed;
         private int gamesWon;
+        private string s = "No games played";
+        public float MaxScore
+        {
+            get { return maxScore; }
+            set { }
+        }
+        public string Name { get; }
+        public float RateOfSuccess
+        {
+            get
+            {
+                return gamesWon / gamesPlayed;
+            }
+        }
+
+        public void AddGame(bool win)
+        {
+            Console.WriteLine("One more game to the player");
+            if (win == true)
+            {
+                gamesWon++;
+                gamesPlayed++;
+            }
+            else
+            {
+                gamesPlayed++;
+            }
+        }
+        public PlayerInfo(string playerName)
+        {
+            name = playerName;
+        }
+
     }
 
 }
